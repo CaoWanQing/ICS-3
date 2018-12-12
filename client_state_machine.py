cc5ba559f085bc783af2671d9c97bf6453b95b6f
CaoWanQing <43959936+CaoWanQing@users.noreply.github.com>
2018-12-13 06:48:40 +0800

    Update client_state_machine.py

--- client_state_machine.py
+++ client_state_machine.py
@@ -305,7 +305,7 @@ class ClientSM:
             if len(peer_msg) > 0:
                 peer_msg = json.loads(peer_msg)
                 if peer_msg["action"] == "gamble":
-                    self.out_msg += "You " + peer_msg["status"] + "! You now have " + str(peer_msg["money"]) + "kuai \n"
+                    self.out_msg += "You " + peer_msg["status"] + "! You now have " + str(peer_msg["money"]) + " kuai \n"
 
 #==============================================================================
 # invalid state
